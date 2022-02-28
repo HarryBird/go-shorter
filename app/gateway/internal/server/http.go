5dbda0daa688925e4e3f9ccfbfb245b0396c7fef
Harry <harry.tianhai@gmail.com>
2022-02-28 16:33:30 +0800

    feat: add api DecodeURL  1. 针对http 302 的response encoder

--- app/gateway/internal/server/http.go
+++ app/gateway/internal/server/http.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"fmt"
+	stdhttp "net/http"
+	"strings"
+
 	v1 "github.com/HarryBird/url-shorten/api/gateway/v1"
 	"github.com/HarryBird/url-shorten/app/gateway/internal/conf"
 	"github.com/HarryBird/url-shorten/app/gateway/internal/service"
@@ -30,7 +34,25 @@ func NewHTTPServer(c *conf.Server, serv *service.GatewayService, logger log.Logg
 	if c.Http.Timeout != nil {
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
+
+	// opts = append(opts, http.ResponseEncoder(RedirectResponseEncoder))
+
 	srv := http.NewServer(opts...)
 	v1.RegisterGatewayHTTPServer(srv, serv)
 	return srv
+}
+
+func RedirectResponseEncoder(w stdhttp.ResponseWriter, r *stdhttp.Request, v interface{}) error {
+	fmt.Println("========== RedirectResponseEncoder")
+	fmt.Printf("val: %#v\n", v)
+	if strings.HasPrefix(r.RequestURI, "/v1/url/decode/") {
+		fmt.Println("111")
+		if reply, ok := v.(*v1.DecodeURLReply); ok {
+			fmt.Println("222")
+			stdhttp.Redirect(w, r, reply.Url, 302)
+			return nil
+		}
+	}
+
+	return http.DefaultResponseEncoder(w, r, v)
 }
